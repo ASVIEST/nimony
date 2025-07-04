89daf36b69a2b7051ac18a4c039d67e119ca8a78
ASVIEST <71895914+ASVIEST@users.noreply.github.com>
2025-07-05 00:02:41 +0300

    Cli begins

--- src/nimony/nimsemcyclic.nim
+++ src/nimony/nimsemcyclic.nim
@@ -1,7 +1,7 @@
 ## nimsemcyclic is part of semantic checker that used
 ## before nimsem. It working for cyclic import group (realy SCC)
 ## Unfortunately, this requires restarting on the entire SCC,
-## but fortunately SC is usually small (maximum of 3-4 modules)
+## but fortunately SCC is usually small (maximum of 3-4 modules)
 ## tasks:
 ## 1. Apply some required sem (recursively):
 ##      analyzes the declarations for Types, When stategments, Consts, Procs (currently only signature)
@@ -30,3 +30,80 @@
 ##    and you have [a.buz, b.bar, a.foo] - correct initalize list
 ##    create .init.nif
 ##    final.
+
+import std / [parseopt, strutils, assertions]
+
+proc processScc(filenames: seq[string], outputFilename: string) =
+  discard
+
+type
+  Command = enum
+    cmdHelp
+    cmdVersion
+    cmdRun
+
+proc writeVersion() =
+  echo "nimsemcyclic 0.1"
+  quit(0)
+
+proc writeHelp() =
+  echo """nifface - Nimony cyclic dependency resolver
+
+Usage:
+  nimcyclicsem [options] <command> [module1.1.nif] [module2.1.nif] ...
+
+Commands:
+  run [module1.1.nif] [module2.1.nif]  Resolves cyclic dependend things
+  help, -h                             Show this help
+  version, -v                          Show version
+
+Options:
+  -o, --output:<file> output file for SCC info
+
+Examples:
+  nimcyclicsem run mymodule.1.max_iface.nif mymodule.2.max_iface.nif
+"""
+  quit(0)
+
+proc handleCmdLine() =
+  var
+    cmd = cmdHelp
+    sccFiles: seq[string] = @[]
+    outputFile = ""
+  
+  for kind, key, val in getopt():
+    case kind
+    of cmdArgument:
+      case key.normalize
+      of "run": cmd = cmdRun
+      else:
+        sccFiles.add key
+
+    of cmdLongOption, cmdShortOption:
+      case key.normalize
+      of "help", "h": cmd = cmdHelp
+      of "version", "v": cmd = cmdVersion
+      of "output", "o":
+        outputFile = val
+      else:
+        echo "Unknown option: --", key
+        quit(1)
+    of cmdEnd: assert false, "cannot happen"
+  
+  case cmd
+  of cmdVersion: writeVersion()
+  of cmdHelp: writeHelp()
+  of cmdRun: 
+    if sccFiles.len == 0:
+      echo "At least one file in SCC required"
+      quit(1)
+    
+    if outputFile == "":
+      echo "Output file required"
+      quit(1)
+    
+    processScc(sccFiles, outputFile)
+
+
+when isMainModule:
+  handleCmdLine()
